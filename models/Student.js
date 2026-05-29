7d2a696e7c8debe11cd4d31834840006d6bd8271
fred olotoin <43315244+freduck@users.noreply.github.com>
2026-05-29 13:06:04 +0100

    Refactor Student model to use connection variable

--- models/Student.js
+++ models/Student.js
@@ -4,54 +4,31 @@
 
 
 
-this.conn = mongoose.createConnection(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-
-  const studentSchema = new mongoose.Schema({
-    name:String,
-    address:String,
-    class:String,
-    username:String,
-    email:String,
-    tel:String,
-    password:String,
-    image:String
-  });
- const Student = mongoose.model('Student',studentSchema);
- module.exports = Student;
-
-
-
-
-
 
 
 
 
 
 //network connection
-// const conn = mongoose.createConnection('mongodb+srv://school:school@cluster0.fl8is4a.mongodb.net/delambo', {
-//   // useNewUrlParser: true,
+ const conn = mongoose.createConnection('mongodb+srv://school:school@cluster0.fl8is4a.mongodb.net/delambo', {
+// useNewUrlParser: true,
 //   // useUnifiedTopology: true,
-// });
+ });
 
-// const studentSchema = new mongoose.Schema({ 
+ const studentSchema = new mongoose.Schema({ 
 
 
-// name:String,
-//     address:String,
-//     class:String,
-//     username:String,
-//     email:String,
-//     tel:String,
-//     password:String,
-//     image:String
+ name:String,
+     address:String,
+    class:String,
+     username:String,
+     email:String,
+    tel:String,
+     password:String,
+     image:String
 
-//  });
-// const Student = conn.model('Student', studentSchema);
+  });
+ const Student = conn.model('Student', studentSchema);
 
-// module.exports = Student;
+ module.exports = Student;
 
