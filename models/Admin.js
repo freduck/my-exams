d35be0c6356f786ee9ec92b20f88475d3ee40a5f
fred olotoin <43315244+freduck@users.noreply.github.com>
2026-06-02 18:53:07 +0100

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
-const mongoose = require ('mongoose');
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+const mongoose = require('mongoose');
+
+// Establish the specific connection first
+const conn = mongoose.createConnection(process.env.MONGO_URI);
 
 const AdminSchema = new mongoose.Schema({
-	name:String,
-	email:String,
-	username:String,
-	password:String
+  name: String,
+  email: String,
+  username: String,
+  password: String
 });
 
-const Admin = conn.model('Admin',AdminSchema);
+// Now conn.model will work perfectly
+const Admin = conn.model('Admin', AdminSchema);
 
 module.exports = Admin;
-
-
-
-
-// const conn = mongoose.createConnection('mongodb+srv://school:school@cluster0.fl8is4a.mongodb.net/delambo', {
-//   // useNewUrlParser: true,
-//   // useUnifiedTopology: true,
-// });
