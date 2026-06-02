7d404335e8cb93556e825bc4c754afa95883bc48
fred olotoin <43315244+freduck@users.noreply.github.com>
2026-06-02 18:50:01 +0100

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,25 +1,8 @@
 require('dotenv').config();
 const mongoose = require ('mongoose');
-// mongoose.connect('mongodb://localhost:27017/delambo',{}).then(() => console.log('Connected to MongoDB'))
-// .catch((err) => console.error('Error connecting to MongoDB:', err));
-// const mongoose = require('mongoose');
-// const mongoose = require('mongoose');
-
-
-
- const conn = mongoose.createConnection('mongodb+srv://school:school@cluster0.fl8is4a.mongodb.net/delambo', {
-   // useNewUrlParser: true,
-   // useUnifiedTopology: true,
- });
-
-
-
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 const AdminSchema = new mongoose.Schema({
 	name:String,
@@ -28,7 +11,7 @@ const AdminSchema = new mongoose.Schema({
 	password:String
 });
 
-const Admin = mongoose.model('Admin',AdminSchema);
+const Admin = conn.model('Admin',AdminSchema);
 
 module.exports = Admin;
 
