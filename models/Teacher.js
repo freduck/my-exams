365024278d8771f44c8632e99a9c6827723629ff
fred olotoin <43315244+freduck@users.noreply.github.com>
2026-06-03 17:18:32 +0100

    Update Teacher.js

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,1 +1,10 @@
-hi
+const teacherSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  subject: String,
+  qualification: String,
+  phone: String,
+  password: String
+});
+
+module.exports = mongoose.model('Teacher', teacherSchema);
