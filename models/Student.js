cbe9931467f4c08370552f60ac31b2daabb1a9a3
fred olotoin <43315244+freduck@users.noreply.github.com>
2026-06-05 16:05:15 +0100

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -19,12 +19,7 @@ mongoose.connect(uri, {
   useUnifiedTopology: true
 });
 
-mongoose.connection.on('connected', () => {
-  console.log("✅ Connected to MongoDB");
-});
-mongoose.connection.on('error', err => {
-  console.error("❌ MongoDB connection error:", err);
-});
+
 
 // Define schema + model
 const studentSchema = new mongoose.Schema({
