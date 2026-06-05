bbbfd8576e014e9315646ca0a446131c74fb6c4c
fred olotoin <43315244+freduck@users.noreply.github.com>
2026-06-05 16:43:42 +0100

    Update Score.js

--- models/Score.js
+++ models/Score.js
@@ -1,21 +1,20 @@
+// Score.js
 const mongoose = require('mongoose');
-//const Connection = require('./Connection');
-const Connection = require('./Connection'); 
-const db = new Connection(); // This works if exported as class
+const Connection = require('./Connection');
 
-
-
-
+// Instantiate the class
+const db = new Connection(); 
 
 const scoreSchema = new mongoose.Schema({
-	student_name:String,
-	subject:String,
-	score:Number,
-	type:String,
-	totalQuestions:Number,
+	student_name: String,
+	subject: String,
+	score: Number,
+	type: String,
+	totalQuestions: Number,
 	percentage: String
 });
 
-const Score = db.model('Score',scoreSchema);
+// Now you can call .model() directly on your 'db' instance
+const Score = db.model('Score', scoreSchema);
 
 module.exports = Score;
